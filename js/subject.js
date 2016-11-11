7385d47233005b726345a63c9bcfdd6d30b5a4fb
tsw6 <thetswe@gmail.com>
2016-11-11 16:35:06 +0800

    Add sample subject page

---
+++ js/subject.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+	// side-loading of menu items
+	$(function(){	
+		$("#navbar").load("hbg_menu.html"); 
+	});
+	
+	
+	$('#fullpage').fullpage({
+		verticalCentered: true,
+		anchors: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th'],
+		sectionsColor: ['#f5f5ff', '#f5f5f5', '#f5f5f5', '#f5f5f5', '#f5f5f5', '#f5f5f5', '#f5f5ff'],
+		navigation: true,
+		navigationPosition: 'right'
+	});
+	
+		// navigationTooltips: ['Cover', 'Second page', 'Third and last page']
+});
