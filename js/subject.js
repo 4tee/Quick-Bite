1175d9a030b96980e8b3611008be4fab13e7f644
tsw6 <thetswe@gmail.com>
2016-12-07 10:04:12 +0800

    Remove full page setAllowScrolling control

--- js/subject.js
+++ js/subject.js
@@ -18,11 +18,11 @@ $(document).ready(function() {
 		verticalCentered: false,
 		anchors: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', 
 		'11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', 
-		'21st', '22nd', '23rd', '24th'],
+		'21st', '22nd', '23rd'],
 		navigationTooltips: [
 			'', '', '', '', '', '', '', '', '', '', 
 			'', '', '', '', '', '', '', '','', '',
-			'', '', '', ''],
+			'', '', ''],
 		navigation: true,
 		showActiveTooltip: true,
 		navigationPosition: 'right',
@@ -43,7 +43,7 @@ $(document).ready(function() {
 		
 	});
 	
-	 $(document).on("click", ".openModal", function() {
-	 	 $.fn.fullpage.setAllowScrolling(false);
-	 });
+	 // $(document).on("click", ".openModal", function() {
+// 	 	 $.fn.fullpage.setAllowScrolling(false);
+// 	 });
 });
